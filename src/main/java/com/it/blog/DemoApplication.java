71638526773de2f71cddc6a646fadc317b7bce09
W9000183 <18576756475@myoas.com>
2020-07-02 17:41:02 +0800

    【问题描述】:代码提交 【程序描述】:无(原因分析或者是对修改的技术性描述) 【修改内容】: 1. 【相关单号】:Otest-3798 【需要测试】:是

--- src/main/java/com/it/blog/DemoApplication.java
+++ src/main/java/com/it/blog/DemoApplication.java
@@ -25,6 +25,6 @@ public class DemoApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
+        System.out.println("OKOK");
     }
 }
